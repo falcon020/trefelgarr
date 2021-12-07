bdf6304ec2f3eeab2f2f9e0b8828e02c94a2720c
falcon020 <85213564+falcon020@users.noreply.github.com>
2021-12-07 23:19:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function openbar(){
+	let sidebar = document.getElementById("sidebar");
+	sidebar.style.right = "0";
+}
+function closebar(){
+	let sidebar = document.getElementById("sidebar");
+	sidebar.style.right = "-350px";
+}
+
